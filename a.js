7f05b17c6fd09ae997a45a6fe0adf0de384a10a7
ZX <87819234+ZXASQWEDF@users.noreply.github.com>
2021-12-15 21:47:51 +0800

    Add files via upload

---
+++ a.js
@@ -0,0 +1,36 @@
+const loginButton = document.getElementById('login')
+const accountInput = document.getElementById('account')
+const passwordInput = document.getElementById('password')
+const resultOutput = document.getElementById('result')
+
+function login() {
+    resultOutput.style.color = '#00f'
+    resultOutput.innerText = 'Login..'
+    setTimeout(function() {
+        const accountValue = accountInput.value
+        passwordValue = passwordInput.value
+        if (accountValue === '123456' &&
+            passwordValue === '123456') {
+            resultOutput.style.color = '#0f0'
+            window.location.href = '/Volumes/NO NAME/家人/b.html';
+        } else
+        if (accountValue === 'hoikahei' &&
+            passwordValue === '070608') {
+            resultOutput.style.color = '#0f0'
+            window.location.href = 'https://sites.google.com/view/hoikahei/%E4%BB%A3%E6%89%93%E7%B3%BB%E7%B5%B1';
+        } else {
+            resultOutput.style.color = '#f00'
+            resultOutput.innerText = '帳號或密碼不正確'
+        }
+    }, 2000)
+}
+
+//防止页面后退
+history.pushState(null, null, document.URL);
+window.addEventListener('popstate', function() {
+    history.pushState(null, null, document.URL);
+});
+
+
+
+loginButton.addEventListener('click', login) // JavaScript Document
---
+++ jssas.js
@@ -0,0 +1,40 @@
+type = "text/javascript"
+window.onload = function() {
+    var timer = null;
+    var show = document.getElementById("show");
+
+    function show_date_time() {
+        var target = new Date("2022/8/8 11:30:00");
+        var today = new Date();
+        var timeold = (target.getTime() - today.getTime());
+        var sectimeold = timeold / 1000
+        var secondsold = Math.floor(sectimeold);
+        var msPerDay = 24 * 60 * 60 * 1000
+        var e_daysold = timeold / msPerDay
+        var daysold = Math.floor(e_daysold);
+        var e_hrsold = (e_daysold - daysold) * 24;
+        var hrsold = Math.floor(e_hrsold);
+        var e_minsold = (e_hrsold - hrsold) * 60;
+        var minsold = Math.floor((e_hrsold - hrsold) * 60);
+        var seconds = Math.floor((e_minsold - minsold) * 60);
+        if (daysold < 0) {
+            window.location.href = "https://zxasqwedf.github.io/1/ppp.html";
+            clearInterval(timer);
+        } else {
+            if (daysold < 10) {
+                daysold = "0" + daysold
+            }
+            if (hrsold < 10) {
+                hrsold = "0" + hrsold
+            }
+            if (minsold < 10) {
+                minsold = "0" + minsold
+            }
+            if (seconds < 10) {
+                seconds = "0" + seconds
+            }
+            show.innerHTML = "距離复查時間還有:" + daysold + "天" + hrsold + "小時" + minsold + "分" + seconds + "秒";
+        }
+    }
+    timer = setInterval(show_date_time, 1000);
+}
