053e0eab31254a0e497fe896a2e5d13605ca8aef
ZX <87819234+ZXASQWEDF@users.noreply.github.com>
2021-12-16 21:17:19 +0800

    Add files via upload

--- c.js
+++ c.js
@@ -1,46 +1,9 @@
-function CheckPassword(Password) {
-    if (Password != "leewn") {
-        alert("密碼錯誤!")
-    } else {
-        window.open("https://zxasqwedf.github.io/2/复查日期.html")
+loopy()
+
+function loopy() {
+    var sWord = ""
+    while (sWord != "123") { //初始密码123
+        sWord = prompt("输入正确密码才能登陆!")
     }
+    window.location.href = 'https://zxasqwedf.github.io/2/b.html';
 }
-$http({
-    method: 'POST',
-    url: '/test',
-    params: {
-        cycle: key,
-        emp_id: user.id
-    }
-}).success(function(resp) {
-    //  处理逻辑
-});
-$http({
-    method: 'POST',
-    url: '/test',
-    params: {
-        cycle: key,
-        emp_id: user.id
-    },
-    //  新增content-type头部属性
-    heads: {
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-}).success(function(resp) {
-    //  处理逻辑
-});
-$http({
-    method: 'POST',
-    url: '/test',
-    //  以表单方式提交，将Object转换为form参数方式
-    data: $httpParamSerializer({
-        cycle: key,
-        emp_id: user.id
-    }),
-    //  新增content-type头部属性
-    heads: {
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-}).success(function(resp) {
-    //  处理逻辑
-});
